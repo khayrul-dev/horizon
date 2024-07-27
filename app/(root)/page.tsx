19d606c32a05783a844cee26299c493ebf95c189
Khayrul Islam <khayrulislam2525@gmail.com>
2024-07-27 18:32:40 +0600

    main layout complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,34 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedin = {
+    firstName: "Adrian",
+    lastName: "JSM",
+    email: "contact@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            subtext="Access and manage your account and transactions efficiently"
+            user={loggedin?.firstName || "Guest"}
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+      <RightSidebar user={loggedin} transactions={[]} banks={[]} />
+    </section>
+  );
 };
 
 export default Home;
